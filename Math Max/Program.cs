f5552a9f28af6d219cf131e236dc4f4f07d4968a
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-13 17:19:26 +0200

    Add project files.

---
+++ Math Max/Program.cs
@@ -0,0 +1,26 @@
+﻿//сумиране на числа и изписване на най-голямото
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Math Max 
+        Console.WriteLine("Write the numbers that you need to sum!");
+
+
+        int max = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int z = int.Parse(Console.ReadLine());
+
+
+        int largest = Math.Max(max, Math.Max(b, Math.Max(c, Math.Max(d, Math.Max(x, Math.Max(y, z))))));
+
+
+        Console.WriteLine("The largest number is: " + largest);
+    }
+}
